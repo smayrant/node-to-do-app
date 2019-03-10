a7e098a6d81a59f16b0619792f9862fc8e87c22b
Sheldrick <mrmayrant@yahoo.com>
2019-03-09 19:58:57 -0500

    added the basic functionality to add and delete items from an array

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,9 +1,24 @@
+const bodyParser = require('body-parser');
+
+let data = [{ item: 'get milk' }, { item: 'get juice' }, { item: 'go to gym' }];
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 module.exports = function(app) {
   app.get('/todo', function(req, res) {
-    res.render('todo');
+    res.render('todo', { todos: data });
   });
 
-  app.post('/todo', function(req, res) {});
+  app.post('/todo', urlencodedParser, function(req, res) {
+    //   data from the body will be pushed onto the data array
+    data.push(req.body);
+    //   response will be sent back as json
+    res.json(data);
+  });
 
-  app.delete('/todo', function(req, res) {});
+  app.delete('/todo/:item', function(req, res) {
+    data = data.filter(function(todo) {
+      return todo.item.replace(/ /g, '-') !== req.params.item;
+    });
+    res.json({ todos: data });
+  });
 };
